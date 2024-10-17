c46b9ab4eb672c4b5e92163b7aea87ab423514e3
BruceWayne2024 <160827138+BruceWayne2024@users.noreply.github.com>
2024-10-17 20:32:37 +0530

    Update index.js

--- index.js
+++ index.js
@@ -18,7 +18,7 @@ const baseDomains = [
   "osakastreetbites.services",
   "shoppiejapanclothes.info",
   "shinkansenfood.shop",
-  "hikokilove.us",
+  "hikokilove.online",
 ];
 
 const generateAllowedUrls = (domains) => {
